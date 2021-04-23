b15a93292013c70d9b40a1c330f0030429afd45a
Hajer Thabet <hajerthabet10@gmail.com>
2021-04-23 19:48:41 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,10 @@
-# Use this space to code along with the readme
+# Use this space to code along with the read puts phrase
+phrase = "Hello World!"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
 
-def greeting
-  phrase = "Hello World!"
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-end
-greeting
+
